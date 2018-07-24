03254fe902921cdedc5946dc836833fb1850b677
David Ross <david.w.ross2@gmail.com>
2018-07-24 10:03:44 -0700

    Part way through Colleges page

--- Assignment_3/assign3/colleges.js
+++ Assignment_3/assign3/colleges.js
@@ -15,3 +15,24 @@ var univArray = new Array(
 		{name: "Santa Clara University", nickname: "SCU", ownership: "private", SATh: 1380, SATl: 1190, tuition: 43812},
 		{name: "Mills College", nickname: "Mills College", ownership: "private", SATh: 1250, SATl: 1040, tuition: 42918}
 		);
+
+var listitems = "";
+
+document.getElementById("resultsTableElemnts").innerHTML = "<tr class='tableHeaderRow'><td>Name</td><td>SAT High</td><td>SAT Low</td><td>Tuition</td></tr>";
+
+function addItem() {
+	var elem = document.getElementById("resultsTableElemnts");
+
+	if (typeof elem != 'undefined')
+	{
+		document.getElementById("resultsTableElemnts").innerHTML = "";
+	}
+	listitems = listitems + "<tr class='tableHeaderRow'><td>Name</td><td>SAT High</td><td>SAT Low</td><td>Tuition</td></tr>";
+	listitems = listitems + "<tr><td>" + univArray[0].nickname + "</td><td>" + univArray[0].SATh + "</td><td>" + univArray[0].SATl + "</td><td>" + univArray[0].tuition + "</td></tr>";
+	listitems = listitems + "<tr><td>" + univArray[1].nickname + "</td><td>" + univArray[1].SATh + "</td><td>" + univArray[1].SATl + "</td><td>" + univArray[1].tuition + "</td></tr>";
+	listitems = listitems + "<tr><td>" + univArray[6].nickname + "</td><td>" + univArray[6].SATh + "</td><td>" + univArray[6].SATl + "</td><td>" + univArray[6].tuition + "</td></tr>";
+	document.getElementById("resultsTableElemnts").innerHTML = listitems;
+}
+
+var button = document.getElementById("updateTableButton");
+button.addEventListener("click",addItem,false);
