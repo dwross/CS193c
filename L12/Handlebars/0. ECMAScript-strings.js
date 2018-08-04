ecdfbe006e72d22e47130fd35f6c04e83a3d3905
David Ross <david.w.ross2@gmail.com>
2018-08-04 10:30:47 -0700

    Added l12

---
+++ L12/Handlebars/0. ECMAScript-strings.js
@@ -0,0 +1,15 @@
+////// Template Strings //////
+
+var ex1 = `an example`;
+var ex2 = `another
+example across lines`;
+
+/// expression evaluation in Template Strings
+
+var amount = 5;
+var price = 10;
+
+var ex3 = `you ordered ${amount} items`;
+
+var ex4 = `ordering ${amount} items, total cost of \$${amount * price}`;
+
